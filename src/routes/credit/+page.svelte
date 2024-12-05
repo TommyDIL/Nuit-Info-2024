455253a298f5fb493c8aa2007e40ff7c51c91cd3
moiroudelliott <73348672+moiroudelliott@users.noreply.github.com>
2024-12-06 00:20:00 +0100

    added: pictures

--- src/routes/credit/+page.svelte
+++ src/routes/credit/+page.svelte
@@ -1,18 +1,49 @@
 <div class="overflow-hidden flex items-center justify-center h-screen bg-back_cath">
-    <div class="animate-upcredits max-w-[300px] text-center text-2xl space-y-[1.6rem]">
-      <!-- Texte principal -->
-      <p class="text-hack font-vt323 text-xl">This is a game</p>
-  
-      <!-- Section intermédiaire -->
-      <div class="p-20 bg-slate-400">
-        <p class="text-blue-700">test</p>
-      </div>
-  
-      <!-- Texte final avec le strong -->
-      <div>
-        <strong class="block text-lg font-normal">Executive Producer</strong>
-        Brad Pitt
-      </div>
+    <div class="animate-upcredits max-w-[800px] text-center text-2xl space-y-[1.6rem]">
+        <div>
+            <h1 class="vt323-regular">NOTRE EQUIPE : </h1>
+            <div class="flex flex-row p-5 items-center justify-center">
+                <div class="m-5">
+                    <p class="vt323-regular">Elliott MOIROUD</p>
+                    <img src="/src/images/elliott.jpg" class="w-40 h-40 border-dashed border-red-500 border-2">
+                </div>
+                <div class="m-5">
+                    <p class="vt323-regular">Julie TROGON</p>
+                    <img src="/src/images/julie.jpg" class="w-40 h-40 border-dashed border-red-500 border-2">
+                </div>
+                <div class="m-5">
+                    <p class="vt323-regular">Tommy Di LUNA</p>
+                    <img src="/src/images/tommy.jpg" class="w-40 h-40 border-dashed border-red-500 border-2">
+                </div>
+            </div>
+            <div class="flex flex-row p-5 items-center justify-center">
+                <div class="m-5">
+                    <p class="vt323-regular">Hugo FERUCCI</p>
+                    <img src="/src/images/hugo.jpg" class="w-40 h-40 border-dashed border-red-500 border-2">
+                </div>
+                <div class="m-5">
+                    <p class="vt323-regular">Chloé FAUCON</p>
+                    <img src="/src/images/chloe.jpg" class="w-40 h-40 border-dashed border-red-500 border-2">
+                </div>
+                <div class="m-5">
+                    <p class="vt323-regular">Martin ARNOUX</p>
+                    <img src="/src/images/martin.jpg" class="w-40 h-40 border-dashed border-red-500 border-2">
+                </div>
+            </div>
+            <div class="flex flex-row p-5 items-center justify-center">
+                <div class="m-5">
+                    <p class="vt323-regular">Natal FEVRE-BURDY</p>
+                    <img src="/src/images/natal.jpg" class="w-40 h-40 border-dashed border-red-500 border-2">
+                </div>
+                <div class="m-5">
+                    <p class="vt323-regular">Teva PHILIPPE</p>
+                    <img src="/src/images/teva.jpg" class="w-40 h-40 border-dashed border-red-500 border-2">
+                </div>
+                <div class="m-5">
+                    <p class="vt323-regular">Etienne MALABRE</p>
+                    <img src="/src/images/etienne.jpg" class="w-40 h-40 border-dashed border-red-500 border-2">
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-  
+</div>

09ef7d91dd3c3e9cdd159aa02901df0e263b948b
moiroudelliott <73348672+moiroudelliott@users.noreply.github.com>
2024-12-05 23:40:02 +0100

    new page: credit

---
+++ src/routes/credit/+page.svelte
@@ -0,0 +1,18 @@
+<div class="overflow-hidden flex items-center justify-center h-screen bg-back_cath">
+    <div class="animate-upcredits max-w-[300px] text-center text-2xl space-y-[1.6rem]">
+      <!-- Texte principal -->
+      <p class="text-hack font-vt323 text-xl">This is a game</p>
+  
+      <!-- Section intermédiaire -->
+      <div class="p-20 bg-slate-400">
+        <p class="text-blue-700">test</p>
+      </div>
+  
+      <!-- Texte final avec le strong -->
+      <div>
+        <strong class="block text-lg font-normal">Executive Producer</strong>
+        Brad Pitt
+      </div>
+    </div>
+  </div>
+  
--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,3 +1,3 @@
-<div class="bg-back_cath absolute top-0 bottom-0 left-0 right-0 p-20">
+<div class="bg-back_cath absolute top-0 bottom-0 left-0 right-0 p-20 flex justify-center">
     <p class="text-hack vt323-regular text-xl">This is a game</p>
 </div>

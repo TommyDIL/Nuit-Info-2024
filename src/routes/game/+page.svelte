3e7306fc682925fb2e5f0133d2697c1ae4c5740d
jtrognon <julietrognon74@gmail.com>
2024-12-06 03:01:14 +0100

    potable

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -43,32 +43,34 @@
         
         <div class=" absolute bottom-0">
 
-            <div class="bg-back_cath absolute -z-10 inset-0 mx-0 " >
-                
-            </div>
-
-            <div class=" absolute bg-back_cath text-hack vt323-regular text-xl h-72 ml-32 w-3/4 text-center mt-20" id="text-container">
+            <div class="absolute inset-0 my-20 mx-40">
+                <div class="absolute bg-back_cath -z-10 inset-0 mx-20" >
                     
-    
-                <button on:click={addFish} class="z-40">Pêcher</button>
+                </div>
                 
-
-                <p>Nombre de poissons : {nbFish}</p>
-
-                <p>Argent : {money}€</p>
-
-                <p>Prix du poisson : {Math.round(moneyPerFish * 100) / 100}€</p>
-                <p>Nombre de pêcheurs : {nbFisherman}</p>
-                <button on:click={buyFisherman}>Achat d'un pêcheur ({fishermanPrice}€)</button>
+                <div class=" absolute text-hack vt323-regular text-xl inset-0 ml-60 mt-20" id="text-container">
+                    
+                    
+                    <button on:click={addFish} class="z-40">Pêcher</button>
+                    
+                    
+                    <p>Nombre de poissons : {nbFish}</p>
+                    
+                    <p>Argent : {money}€</p>
+                    
+                    <p>Prix du poisson : {Math.round(moneyPerFish * 100) / 100}€</p>
+                    <p>Nombre de pêcheurs : {nbFisherman}</p>
+                    <button on:click={buyFisherman}>Achat d'un pêcheur ({fishermanPrice}€)</button>
+                </div>
             </div>
-
+                
             <img class="z-20 pointer-events-none" src="/src/images/tv.png" alt="AVENGERSSS">
         </div>
 
 
     </div>
     
-    <div class="bg-red-300 w-1/">
+    <div class="bg-red-300 w-1/6">
         <p>dedede</p>
     </div>
 </div>

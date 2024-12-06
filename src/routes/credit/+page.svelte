c32f05ff50294159730a23af5818419b45bcb646
skynat74 <skynat74@gmail.com>
2024-12-06 03:05:29 +0100

    fix: color button credit

--- src/routes/credit/+page.svelte
+++ src/routes/credit/+page.svelte
@@ -73,8 +73,8 @@ function changeColor() {
 </script>
 
 <button on:click={changeColor} type="button" class="absolute m-3
-text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium 
-rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+text-white focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium 
+rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-400 dark:hover:bg-gray-500">
   <img src="/src/images/changeColor.png" alt="Change Color" class="h-8 ">
 </button>
 
